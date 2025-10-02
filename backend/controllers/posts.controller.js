12e6d176cedd10bb211846260bf566a87238dc57
Shubham jakate <shubhamjakate0505@gmail.com>
2025-10-02 10:32:51 +0530

    Post-Route

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -2,8 +2,30 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcrypt'
 import Profile from '../models/profile.model.js';
+import Post from '../models/posts.model.js'
 
 export const activeCheck=async(req,res)=>{
     return res.status(200).json({message:"Running"})
 }
 
+export const createPost=async(req,res)=>{
+    const {token}=req.body;
+    try{
+        const user=await User.findOne({token});
+        if(!user) return res.status(400).json({message:"User not Found"});
+
+        const post=new Post({
+            userID:user._id,
+            body:req.body.body,
+            media:req.file !=undefined ?req.file.filename:"",
+            filetypes:req.file !=undefined ? req.file.mimetype.split("/")[1] :"",
+            active: req.body.active === "true" || req.body.active === true
+        })
+
+        await post.save();
+
+        return res.status(200).json({message:"Post created"});
+    }catch(error){
+        return res.status(500).json({message:error.message});
+    }
+}
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,28 @@
 
 import { Router } from "express";
-import { activeCheck } from "../controllers/posts.controller.js";
+import { activeCheck, createPost} from "../controllers/posts.controller.js";
+import multer from "multer";
+// import React from "react";
+// import { createRef } from "react";
 
 const router=Router();
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null,file.originalname)
+  }
+})
+const upload = multer({ storage: storage });
+
+// router.route("/update_profile_picture")
+// .post(upload.single('profile_picture'),uploadProfilePicture)
+
+
 router.route('/').get(activeCheck);
+
+router.route("/post").post(upload.single('media'),createPost);
 
 export default router;
