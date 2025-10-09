76abff8899e1ac133e28b506195b87102c1982dd
Shubham jakate <shubhamjakate0505@gmail.com>
2025-10-09 17:57:54 +0530

    Authetiction-page

--- my-app/src/pages/login/index.jsx
+++ my-app/src/pages/login/index.jsx
@@ -1,13 +1,52 @@
 import UserLayout from '@/layouts/UserLayout'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/router'
+import styles from './style.module.css'
 
 function loginComponent() {
+ 
+  const authState=useSelector((state)=>state.auth)
+  const router=useRouter();
+  const [userLoginMethod,setUserLoginMethod]=useState(false);
+
+  // const isLoginMethod=useState(false);
+  useEffect(()=>{
+    if(authState.loggedIn){
+      router.push("/home");
+  }})
+
+
+
+
   return (
     <UserLayout>
-      <h2>Login</h2>
-    <p>This is the login page.</p>
+      <div className={styles.container}>
+          <div className={styles.cardContainer}>
+          <div className={styles.cardContainer_left}>
+            <p className={styles.cardleft_heading}>{ userLoginMethod ? "Sing in":"Sing Up"}</p>
+
+              <div className={styles.inputContainer}>
+                <div className={styles.inputRow}>
+                  <input  className={styles.inputFiled}  type="text" placeholder='Username'/>
+                  <input  className={styles.inputFiled}  type="text" placeholder='Name'/>
+                </div>
+                  <input  className={styles.inputFiled}  type="text" placeholder='Email'/>
+
+                  <input  className={styles.inputFiled}  type="text" placeholder='Password'/>
+                <div className={styles.buttonWithOutline}>
+                  <p>{userLoginMethod ?"Sing In":"Sing Up"}</p>
+                </div>
+              </div>
+            
+          </div>
+          <div className={styles.cardContainer_right}></div>
+        </div>
+
+      </div>
     </UserLayout>
   )
+
 }
 
 export default loginComponent
